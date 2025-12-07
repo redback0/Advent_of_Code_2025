59db0f1f769bc1877b58ef64a6f194e9172bfb7d
redback0 <gh@ttpg.xyz>
2025-12-08 05:50:47 +1030

    Make day 7 part 2 print nicely

--- day7/part2/solution.cpp
+++ day7/part2/solution.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <iostream>
 #include <iterator>
+#include <numeric>
 #include <string>
 #include <vector>
 
@@ -32,8 +33,6 @@ int main(int ac, char** av)
         input.good() && !line.empty();
         std::getline(input, line))
     {
-        std::cout << line << std::endl;
-
         auto split = std::find(line.begin(), line.end(), '^');
 
         for (; split != line.end(); split = std::find(split + 1, line.end(), '^'))
@@ -42,26 +41,32 @@ int main(int ac, char** av)
 
             *(beam - 1) += *beam;
             *(beam + 1) += *beam;
-            *beam = 0;
+            *beam = -1;
         }
 
         for (auto& beam : beams)
         {
-            if (beam > 9)
-                std::cout << 'B';
-            else
+            if (beam < 0)
+            {
+                beam = 0;
+                std::cout << '^';
+            }
+            else if (beam == 0)
+                std::cout << ' ';
+            else if (beam < 10)
                 std::cout << beam;
+            else if (beam < 10 + 26)
+                std::cout << char(beam + 'a' - 10);
+            else if (beam < 10 + 26 + 26)
+                std::cout << char(beam + 'A' - (10 + 26));
+            else
+                std::cout << '|';
         }
 
         std::cout << std::endl;
     }
 
-    int64_t total_splits = 0;
+    int64_t total_splits = std::accumulate(beams.begin(), beams.end(), 0);
 
-    for (auto beam : beams)
-    {
-        total_splits += beam;
-    }
-
-    std::cout << total_splits << std::endl;
+    std::cout << std::dec << total_splits << std::endl;
 }
